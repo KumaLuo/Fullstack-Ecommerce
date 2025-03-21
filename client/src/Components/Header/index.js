dafc2408181458133b189ec0634da446798c669e
Zhenxiong Luo <u7768702@anu.edu.au>
2025-03-21 21:59:13 +1100

    Added logo and location button

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -1,3 +1,7 @@
+import logo from '../../assets/images/bacola-logo.png';
+import { Link } from 'react-router-dom';
+import CountryDropdown from '../CountryDropdown';
+
 const Header = () => {
     return (
         <>
@@ -8,6 +12,21 @@ const Header = () => {
                             orders maybe processed with a slight delay</p> 
                     </div>
                 </div>
+
+                <div className="header">
+                    <div className="container">
+                        <div className="row">
+
+                            <div className="logowrapper d-flex align-items-center col-sm-2">
+                                <Link to="/"> <img src={logo} alt="Logo" /> </Link>
+                            </div>
+
+                            <div className="col-sm-10 d-flex align-items-center part2">
+                                <CountryDropdown />
+                            </div>
+                        </div>
+                    </div>
+                </div>    
             </div>
         </>
     );

48a7187f266ead47cdca6df48801b8cde67793da
Zhenxiong Luo <u7768702@anu.edu.au>
2025-03-21 18:48:29 +1100

    Deleted redundant files and installed basic dependency.

---
+++ client/src/Components/Header/index.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <>
+            <div className="headerwrapper">
+                <div className="top-strip bg-blue">
+                    <div className="container"> 
+                        <p className="mb-0 mt-0 text-center">Due to the <b> COVID 19 </b>  epidemic,
+                            orders maybe processed with a slight delay</p> 
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;

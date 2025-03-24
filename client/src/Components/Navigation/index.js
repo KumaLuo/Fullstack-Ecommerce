d93ca5da16c920abd495e1fc726e90e4baf1b383
Zhenxiong Luo <u7768702@anu.edu.au>
2025-03-24 20:22:29 +1100

    changed navbar from link to link-button

--- client/src/Components/Navigation/index.js
+++ client/src/Components/Navigation/index.js
@@ -16,32 +16,32 @@ const Navigation = () => {
     return(
         <nav>
             <div className="container">
-                <div className="row">
+                <div className="row align-items-center">
 
                     <div className="col-sm-3 navPart1">
                         <Button className='allTab align-items-center'>
-                            <span className="icon1 mr-3"><IoIosMenu /></span>
+                            <span className="icon1 mr-2"><IoIosMenu /></span>
                             <span className='text'>ALL CATEGORIES</span>
-                            <span className="icon2 ml-3"><FaAngleDown /></span>
+                            <span className="icon2 ml-2"><FaAngleDown /></span>
                         </Button>
                     </div>
 
                     <div className="col-sm-9 navPart2 d-flex align-items-center">
                         <ul className="list list-inline ml-auto">
-                            <li className="list-inline-item"> <Link to='/'> <IoHomeOutline/> &nbsp; 
-                            Home </Link> </li>
-                            <li className="list-inline-item"> <Link to='/'> <IoShirtOutline/> &nbsp;
-                            Fashion </Link> </li>
-                            <li className="list-inline-item"> <Link to='/'> <MdOutlineDevices/> &nbsp; 
-                            Electronic </Link> </li>
-                            <li className="list-inline-item"> <Link to='/'> <IoPizzaOutline/> &nbsp; 
-                            Bakery </Link> </li>
-                            <li className="list-inline-item"> <Link to='/'> <LuGrape/> &nbsp;
-                             Grocery </Link> </li>
-                            <li className="list-inline-item"> <Link to='/'> <MdOutlineArticle/> &nbsp;
-                             Blog </Link> </li>
-                            <li className="list-inline-item"> <Link to='/'> <MdOutlineContactSupport/> &nbsp; 
-                            Contact Us</Link> </li>
+                            <li className="list-inline-item"> <Link to='/'> <Button><IoHomeOutline/> &nbsp;
+                            Home </Button> </Link> </li>
+                            <li className="list-inline-item"> <Link to='/'> <Button><IoShirtOutline/> &nbsp;
+                            Fashion</Button></Link> </li>
+                            <li className="list-inline-item"> <Link to='/'> <Button><MdOutlineDevices/> &nbsp; 
+                            Electronic </Button></Link> </li>
+                            <li className="list-inline-item"> <Link to='/'> <Button><IoPizzaOutline/> &nbsp; 
+                            Bakery </Button></Link> </li>
+                            <li className="list-inline-item"> <Link to='/'> <Button><LuGrape/> &nbsp;
+                             Grocery </Button></Link> </li>
+                            <li className="list-inline-item"> <Link to='/'> <Button><MdOutlineArticle/> &nbsp;
+                             Blog </Button></Link> </li>
+                            <li className="list-inline-item"> <Link to='/'> <Button><MdOutlineContactSupport/> &nbsp; 
+                            Contact Us</Button></Link> </li>
                         </ul>
                         
                     </div>

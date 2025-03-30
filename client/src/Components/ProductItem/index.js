3432f5afaaf624a8e8bfb4eeca8e592cdab0fc21
Zhenxiong Luo <u7768702@anu.edu.au>
2025-03-30 20:05:18 +1100

    Completed products row and productItem component

---
+++ client/src/Components/ProductItem/index.js
@@ -0,0 +1,77 @@
+import { React, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import Rating from '@mui/material/Rating';
+import { SlSizeFullscreen } from "react-icons/sl";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+
+
+
+
+const ProductItem = ({ product }) => {
+
+
+    const [likedMap, setLikedMap] = useState({});
+    const toggleLike = (id) => {
+        setLikedMap(prev => ({
+            ...prev,
+            [id]: !prev[id],
+        }));
+    };
+    return (
+        <div className="productItem" id={product.id}>
+            <div className="imgWrap">
+                <img src={product.image} alt={product.name} className="w-100" />
+                <div className="badge-container">
+                    {product.discount && <span className="badge badge-primary">-{product.discount}%</span>}
+                    {product.recommended && <span className="badge badge-recommend">Recommended</span>}
+                    {product.organic && <span className="badge badge-organic">Organic</span>}
+                </div>
+
+                <div className="actionIcons">
+                    <Button className='fullscreen'><SlSizeFullscreen /></Button>
+                    {likedMap[product.id] ? (
+                        <Button className="likeIcon like" onClick={() => toggleLike(product.id)} ><FaHeart /></Button>
+                    ) : (
+                        <Button className="likeIcon unlike" onClick={() => toggleLike(product.id)} ><FaRegHeart /></Button>
+                    )}
+                </div>
+
+            </div>
+            <div className="productInfo">
+                <div className="productName mb-1">
+                    <Link to={`/product/${product.id}`}>{product.name}</Link>
+                </div>
+
+                {product.stock > 0 ? (
+                    <span className="success">In Stock</span>
+                ) : (
+                    <span className="text-danger">Out of Stock</span>
+                )}
+
+                <div className="productRating d-flex align-items-center mt-2">
+                    <Rating
+                        name="read-only"
+                        value={product.rating}
+                        precision={0.5}
+                        readOnly
+                        size="small"
+                    />
+                </div>
+
+                <div className="productPrice mt-2">
+                    {product.oldPrice && <span className="oldPrice mr-2">${product.oldPrice}</span>}
+                    <span className="price">${product.price}</span>
+                </div>
+
+                {product.stock > 0 ? (
+                    <Button className="addToCartBtn mt-2 w-100">Add to Cart</Button>
+                ) : (
+                    <Link to="" className='readmore'><Button className="addToCartBtn mt-2 w-100">Read More</Button></Link>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default ProductItem;
--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,8 +1,161 @@
 import HomeBanner from "../../Components/HomeBanner";
+import Button from '@mui/material/Button';
+import { IoIosArrowRoundForward } from "react-icons/io";
+import React from "react";
+import Slider from "react-slick";
+import ProductItem from "../../Components/ProductItem";
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/pagination';
+// import {Navigation} from 'swiper/modules';
 
 const Home = () => {
+
+  const products = [
+    {
+      id: 1,
+      name: "All Natural Italian-Style Chicken Meatballs",
+      image: "https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg",
+      discount: 20,
+      recommended: true,
+      organic: true,
+      stock: 10,
+      rating: 4.5,
+      oldPrice: 30,
+      price: 24,
+    },
+    {
+      id: 2,
+      name: "Field Roast Chao Cheese Creamy Original",
+      image: "https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-46-346x310.jpg",
+      discount: 19,
+      recommended: false,
+      organic: true,
+      stock: 0,
+      rating: 4.5,
+      oldPrice: 24,
+      price: 19.5,
+    },
+    {
+      id: 3,
+      name: "All Natural Italian-Style Chicken Meatballs",
+      image: "https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg",
+      discount: 20,
+      recommended: true,
+      organic: false,
+      stock: 10,
+      rating: 4.5,
+      oldPrice: 30,
+      price: 24,
+    },
+    {
+      id: 4,
+      name: "All Natural Italian-Style Chicken Meatballs",
+      image: "https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg",
+      discount: 20,
+      recommended: true,
+      organic: false,
+      stock: 10,
+      rating: 4.5,
+      oldPrice: 30,
+      price: 24,
+    },]
+
+  var productSliderOptions = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  };
+
   return (
-    <HomeBanner />
+    <>
+      <HomeBanner />
+      <section className="homeProducts">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3">
+              <div className="banner ">
+                <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/banner-box.jpg" alt="product"
+                  className="cursor w-100" />
+              </div>
+            </div>
+
+            <div className="col-md-9 productRow">
+              <div className="d-flex align-items-center mb-4">
+                <div className="info w-75">
+                  <h3 className="mb-0 hd">BEST SELLERS</h3>
+                  <p className="text-light text-sml mb-0">Do not miss the current offers until the end of March.</p>
+                </div>
+                <Button className="viewAllbtn ml-auto">View All <IoIosArrowRoundForward /></Button>
+              </div>
+
+              <div className="productSlider w-100">
+                <Slider {...productSliderOptions}>
+
+                  {products.map((product) => (
+                    <ProductItem key={product.id} product={product} />
+                  ))}
+                </Slider>
+              </div>
+
+              {/* <div className="productRow w-100">
+                <Swiper
+                  spaceBetween={30}
+                  slidesPerView={4}
+                  pagination={{
+                    clickable: true,
+                  }}
+                  modules={['navigation']}
+                  className="mySwiper"
+                >
+                  <SwiperSlide>
+                    <div className="productItem">
+                      <div className="imgWrap">
+                        <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-62-346x310.jpg" alt="product" className="w-100" />
+                      </div>
+
+                      <div className="productInfo">
+                        <div className="productName mb-1">
+                          <Link to="/product/1">All Natural Italian-Style Chicken Meatballs</Link>
+                        </div>
+
+                        <span className="success">In Stock</span>
+
+                        <div className="productRating d-flex align-items-center mt-2">
+                          <Rating
+                            name="read-only"
+                            value={4.5}
+                            precision={0.5}
+                            readOnly
+                            size="small"
+                          />
+                        </div>
+
+                        <div className="productPrice mt-2">
+                          <span className="oldPrice mr-2">$30.00</span>
+                          <span className="price">$25.00</span>
+                        </div>
+
+                        <Button className="addToCartBtn mt-2 w-100">Add to Cart</Button>
+
+                      </div>
+
+                    </div>
+                  </SwiperSlide>
+
+                </Swiper>
+
+              </div> */}
+
+
+            </div>
+          </div>
+        </div >
+      </section >
+    </>
   );
 }
 

8e3a659dbe358af6dfad04503e7f330a39f745ac
Zhenxiong Luo <u7768702@anu.edu.au>
2025-05-25 15:08:55 +1000

    Added background in sign in page

--- client/src/Pages/SignIn/index.js
+++ client/src/Pages/SignIn/index.js
@@ -1,20 +1,42 @@
+import logo from '../../assets/images/bacola-logo.png';
+
 
 const SignIn = () => {
     return (
-        <div>
-            <h1>Sign In</h1>
-            <form>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
+        <section className="sign-in">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1280 720"
+                fill="none"
+                preserveAspectRatio="xMidYMid slice"
+                className="sign-in__bg-svg"
+            >
+                <rect width="100%" height="100%" fill="#E5E5E5" />
+                <g clipPath="url(#clip0_0_1)">
+                    <rect width="100%" height="100%" fill="#2148C0" />
+                    <circle cy="720" r="362" fill="#264ECA" />
+                    <circle cy="720" r="286" fill="#244BC5" />
+                    <circle cy="720" r="219" fill="#274FC7" />
+                    <path
+                        d="M672.5 139C536.9 151.8 444.667 51 415.5 -1L1279.5 2.5V720.5H1183C884.2 682.5 934.5 531 997 460C1033.5 410.167 1099.5 290.1 1071.5 208.5C1036.5 106.5 842 123 672.5 139Z"
+                        fill="#264ECA"
+                    />
+                </g>
+                <defs>
+                    <clipPath id="clip0_0_1">
+                        <rect width="100%" height="100%" fill="white" />
+                    </clipPath>
+                </defs>
+            </svg>
+
+            <div className="container">
+                <div className="box card p-3 shadow border-0 align-items-center">
+                    <img src={logo} alt="Bacola Logo" className="mb-2 w-75" />
+
+
                 </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" name="password" required />
-                </div>
-                <button type="submit">Sign In</button>
-            </form>
-        </div>
+            </div>
+        </section>
     );
 }
 
